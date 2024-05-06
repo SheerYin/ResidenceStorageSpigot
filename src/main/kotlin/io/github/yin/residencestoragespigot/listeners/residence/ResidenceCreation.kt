f5ec3bae815b30e3aad6df8797ed920eebf5bc35
SheerYin <sheerhuangjinyin@gmail.com>
2024-05-06 17:37:58 +0800

    增加权限数量判断

--- src/main/kotlin/io/github/yin/residencestoragespigot/listeners/residence/ResidenceCreation.kt
+++ src/main/kotlin/io/github/yin/residencestoragespigot/listeners/residence/ResidenceCreation.kt
@@ -3,10 +3,13 @@ package io.github.yin.residencestoragespigot.listeners.residence
 import com.bekvon.bukkit.residence.Residence
 import com.bekvon.bukkit.residence.event.ResidenceCreationEvent
 import io.github.yin.residencestoragespigot.ResidenceStorageSpigotMain
+import io.github.yin.residencestoragespigot.storages.ConfigurationYAMLStorage
 import io.github.yin.residencestoragespigot.storages.ResidenceMySQLStorage
 import io.github.yin.residencestoragespigot.supports.ResidenceInfo
 import io.github.yin.servernamespigot.ServerNameSpigotMain
 import org.bukkit.Bukkit
+import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -15,13 +18,18 @@ import java.util.*
 
 object ResidenceCreation : Listener {
 
+    // 领地创建 前 触发，就算创建失败也会触发
     @EventHandler(priority = EventPriority.NORMAL)
     fun onResidenceCreation(event: ResidenceCreationEvent) {
 
+
+        val player = event.player
+        val names = ResidenceMySQLStorage.getOwnerUUIDResidenceNames(player.uniqueId.toString())
         val residenceName = event.residenceName
-        if (ResidenceMySQLStorage.hasResidenceName(residenceName)) {
+        // 玩家领地总数有没有大于 config 的 residences.amount 权限
+        // 有没有重名
+        if (names.size > numberPermissions(player) || names.contains(residenceName)) {
             event.isCancelled = true
-            return
         }
 
         val residence = event.residence
@@ -30,8 +38,9 @@ object ResidenceCreation : Listener {
         val permissions = residence.permissions
 
         Bukkit.getScheduler().runTaskLaterAsynchronously(ResidenceStorageSpigotMain.instance, Runnable {
-            if (Residence.getInstance().residenceManager.residences.containsKey(residenceName.lowercase(Locale.getDefault()))) {
 
+            // 延迟后才知道领地到底创建成功没有
+            if (Residence.getInstance().residenceManager.residences.containsKey(residenceName.lowercase(Locale.getDefault()))) {
                 val residenceInfo =
                     ResidenceInfo(
                         residenceName,
@@ -45,6 +54,20 @@ object ResidenceCreation : Listener {
             }
         }, 5L)
 
+    }
+
+
+    private fun numberPermissions(player: Player): Int {
+        val section: ConfigurationSection = ConfigurationYAMLStorage.fileConfiguration.getConfigurationSection("residences.amount")!!
+        val map: Map<String,Int> = section.getKeys(false).associateBy({ it }, { section.getInt(it) })
+
+        val sorted = map.entries.sortedByDescending { it.value }
+        for (entry in sorted) {
+            if (player.hasPermission("residencestoragespigot.amount." + entry.key)) {
+                return entry.value
+            }
+        }
+        return 0
     }
 
 }
